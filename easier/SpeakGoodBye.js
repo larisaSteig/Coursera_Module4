4b135a46cadc50dce60ed7950a21c0200cfd9bda
Larisa Steig <67192337+larisaSteig@users.noreply.github.com>
2020-07-09 11:47:41 -0600

    Add files via upload
    Solution for easier version question

--- easier/SpeakGoodBye.js
+++ easier/SpeakGoodBye.js
@@ -23,3 +23,17 @@ function speak(name) {
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+
+(function (window){
+  var helloSpeaker = {};
+
+// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+  var speakWord = "Hello";
+
+// STEP 4: Rewrite the 'speak' function such that it is attached to the
+// helloSpeaker object instead of being a standalone function.
+// See Lecture 52, part 2
+  helloSpeaker.speak = function(name) {
+  console.log(speakWord + " " + name);}
+  window.helloSpeaker = helloSpeaker.speak
+})(window);
